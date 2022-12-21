c8a2870c0621131bf4e60e1feb0f269cd57b8ff8
hirotakasuzuki1219 <suzuki-h574cl@pwri.go.jp>
2022-12-21 10:17:18 +0900

    minor change

--- scripts/pub_to_unity.py
+++ scripts/pub_to_unity.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import Float64
+from pubsub_unity_test.msg import MyMsg
+from datetime import datetime
 
 rospy.init_node('pub')
-pub = rospy.Publisher('UnixTime', Float64 , queue_size=1)
+pub = rospy.Publisher('UnixTime', MyMsg , queue_size=1)
+mymsg_unity = MyMsg()
 
 rate = rospy.Rate(10)
 while not rospy.is_shutdown():
-    now = rospy.get_time()
-    pub.publish(now)
+    mymsg_unity.MsgWithUnity = 1
+    # now = rospy.get_time()
+    pub.publish(mymsg_unity)
     rate.sleep()
 
--- scripts/sub_from_unity.py
+++ scripts/sub_from_unity.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import Float64
+from pubsub_unity_test.msg import MyMsg
+from datetime import datetime
 
 def callback(message):
-    print(message.data)
+    print(message.MsgWithUnity)
 
 if __name__ == "__main__":
     rospy.init_node('sub')
-    sub = rospy.Subscriber('UnixTime', Float64 , callback)
+    sub = rospy.Subscriber('UnixTime', MyMsg , callback)
     rospy.spin()
